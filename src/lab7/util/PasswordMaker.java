2c72dff7aeee26e3e8492842ea94b7d0504a4451
VladuDenisG <vladydenis04@gmail.com>
2026-04-29 00:07:50 +0300

    commit 7.6.2

--- src/lab7/util/PasswordMaker.java
+++ src/lab7/util/PasswordMaker.java
@@ -4,15 +4,29 @@ public class PasswordMaker {
     private static final int MAGIC_NUMBER = 3;
     private static final String MAGIC_STRING;
 
-    private String name;
+    private static PasswordMaker instance;
+    private static int callingCount = 0;
+
+    private String name ;
 
     static {
         StringRandomizer srand = new StringRandomizer();
         MAGIC_STRING = srand.randomString(20);
+
+        instance = new PasswordMaker("SingletonInstance");
     }
 
-    public PasswordMaker(String name) {
+    private PasswordMaker(String name)
+    {
         this.name = name;
+    }
+    public static PasswordMaker getInstance() {
+        callingCount++;
+        return instance;
+    }
+
+    public static int getCallingCounts() {
+        return callingCount;
     }
 
     public String getPassword() {
